c780f8b77474483d0908095eba625d32db5b7ec4
Nicolai Dreves Nielsen <Nicolai_Dreves1994@hotmail.dk>
2020-02-04 10:09:11 +0100

    Tested a main.cs

---
+++ CalculatorMain/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorMain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator.Calculator Calc = new Calculator.Calculator();
+
+            Console.WriteLine($"Adding 2 and 5 gives: {Calc.Add(2, 5)}");
+        }
+    }
+}
