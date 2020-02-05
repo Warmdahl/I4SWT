8a635067aa3c320641d238c0e908028a3a15ff99
Nicolai Dreves Nielsen <Nicolai_Dreves1994@hotmail.dk>
2020-02-05 14:34:54 +0100

    Removed cur_num and changed Accumulator

--- Calculator/Calculator.cs
+++ Calculator/Calculator.cs
@@ -4,25 +4,24 @@ namespace Calculator
 {
     public class Calculator
     {
-        private double cur_num;
         public double Add(double a, double b)
         {
-            return cur_num = a + b;
+            return Accumulator = a + b;
         }
 
         public double Subtract(double a, double b)
         {
-            return cur_num = a - b;
+            return Accumulator = a - b;
         }
 
         public double Multiply(double a, double b)
         {
-            return cur_num = a * b;
+            return Accumulator = a * b;
         }
 
         public double Power(double a, double b)
         {
-            return cur_num = Math.Pow(a, b);
+            return Accumulator = Math.Pow(a, b);
         }
 
         public double Divide(double dividend, double divisor)
@@ -30,11 +29,11 @@ namespace Calculator
             if (divisor == 0.0)
             {
                 Console.WriteLine("You cannot divide by zero, type an actual number");
-                return cur_num = 0.0;
+                return Accumulator = 0.0;
             }
             else
             {
-                return cur_num = dividend / divisor;
+                return Accumulator = dividend / divisor;
             }
         }
 
@@ -43,14 +42,6 @@ namespace Calculator
             return 0.0;
         }
 
-        public double Accumulator(double value)
-        {
-            //double acu_result = cur_num;
-            //get {return cur_num;}
-
-            //private set { cur_num = value; }
-
-            return 0.0;
-        }
+        public double Accumulator { get; private set; }
     }
 }
